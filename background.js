2105534715cb8c3f6ec345f64d308a47e1f97a32
Alex Hetrick <aph1777@rit.edu>
2022-03-07 00:00:44 -0500

    Foundations of the button popup above text

--- background.js
+++ background.js
@@ -2,20 +2,21 @@ let active_tab_id = 0;
 var copied_text = "";
 
 chrome.tabs.onActivated.addListener((tab) => {
-  chrome.tabs.get(tab.tabId, async (current_tab_info) => {
-    active_tab_id = tab.tabId;
+	chrome.tabs.get(tab.tabId, async (current_tab_info) => {
+		active_tab_id = tab.tabId;
 
-    chrome.tabs.executeScript(null, { file: "./textCapture.js" }, () =>
-      console.log("I injected textCapture.js into tab " + active_tab_id)
-    );
-  });
+		chrome.tabs.insertCSS(active_tab_id, { file: "./styles.css" });
+		chrome.tabs.executeScript(null, { file: "./textCapture.js" }, () =>
+			console.log("I injected textCapture.js into tab " + active_tab_id)
+		);
+	});
 });
 
 chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
-  if (request.message === "selected text sent") {
-    chrome.storage.local.get(["key"], (result) => {
-      copied_text = result.key;
-      console.log("RECIEVED: " + copied_text);
-    });
-  }
+	if (request.message === "selected text sent") {
+		chrome.storage.local.get(["key"], (result) => {
+			copied_text = result.key;
+			console.log("RECIEVED: " + copied_text);
+		});
+	}
 });
--- textCapture.js
+++ textCapture.js
@@ -1,12 +1,40 @@
-var text = "";
+// var text = "";
+// var element = null;
+var popup = null;
 
-window.addEventListener("mouseup", function () {
-  text = window.getSelection().toString();
-  if (text !== "") {
-    console.log("SELECTED: " + text);
+window.addEventListener("click", function (e) {
+	var textSelection = this.document.getSelection();
+	var text = textSelection.toString();
 
-    chrome.storage.local.set({ key: text }, () => {
-      chrome.runtime.sendMessage({ message: "selected text sent" });
-    });
-  }
+	// Store selected text in local storage
+	if (text !== "") {
+		console.log("SELECTED: " + text);
+
+		chrome.storage.local.set({ key: text }, () => {
+			chrome.runtime.sendMessage({ message: "selected text sent" });
+		});
+
+		// Create popup button
+		if (popup == null) {
+			popup = document.createElement("div");
+			popup.className = "nn-popup-button";
+			document.body.appendChild(popup);
+			popup.innerText = "test123";
+			popup.setAttribute(
+				"style",
+				"position: fixed; top: 0px; left: 0px; z-index: 2147483642;"
+			);
+		}
+	} else {
+		removePopup();
+	}
 });
+
+window.addEventListener("scroll", function () {
+	removePopup();
+});
+
+function removePopup() {
+	if (popup != null) popup.remove();
+	popup = null;
+}
