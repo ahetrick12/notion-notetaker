aa7b037f7efecd3a6a78658fd242627103524494
Alex Hetrick <aph1777@rit.edu>
2022-03-09 23:39:09 -0500

    Tidied up background.js

--- background.js
+++ background.js
@@ -1,51 +1,38 @@
 var validUrlPattern = /^((http|https|ftp):\/\/)/;
-var copied_text = "";
 
-//console.log("GOOD URL: " + tabs[0].url);
+// Script injection listeners
 chrome.tabs.onActivated.addListener(injectScript);
 chrome.tabs.onCreated.addListener(injectScript);
 chrome.tabs.onUpdated.addListener((tabId, changeInfo) => {
+	// Only inject once page is fully loaded
 	if (changeInfo.status == "complete") {
 		console.log("Loading complete");
 		injectScript();
 	}
 });
 
-chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
-	if (request.message === "selected text sent") {
-		chrome.storage.local.get(["key"], (result) => {
-			copied_text = result.key;
-			console.log("RECIEVED: " + copied_text);
-		});
-	}
-});
-
 function injectScript() {
+	// Get current tab info
 	chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+		// Only inject with valid URLs
 		if (validUrlPattern.test(tabs[0].url)) {
 			let tabId = tabs[0].id;
 
+			// Check if script has already been injected by sending a message
+			// If it is already present it will send back a "yes"
 			chrome.tabs.sendMessage(
 				tabId,
 				{ message: "are_you_there_content_script?" },
 				function (msg) {
 					msg = msg || {};
 
-					if (!chrome.runtime.lastError) {
-					} // Ignore lol
+					// Ignore errors lol
+					if (chrome.runtime.lastError) {
+					}
 
+					// Inject scripts if no response
 					if (msg.status != "yes") {
-						chrome.scripting.insertCSS({
-							target: { tabId: tabs[0].id },
-							files: ["./styles.css"],
-						});
-
-						chrome.scripting.executeScript({
-							target: { tabId: tabs[0].id },
-							files: ["./textCapture.js"],
-						});
-
-						console.log("INJECTED INTO " + tabId);
+						runScriptInjection();
 					}
 				}
 			);
@@ -54,3 +41,27 @@ function injectScript() {
 		}
 	});
 }
+
+function runScriptInjection() {
+	// All injection and insertion processes
+	chrome.scripting.insertCSS({
+		target: { tabId: tabs[0].id },
+		files: ["./styles.css"],
+	});
+
+	chrome.scripting.executeScript({
+		target: { tabId: tabs[0].id },
+		files: ["./textCapture.js"],
+	});
+
+	console.log("INJECTED INTO " + tabId);
+}
+
+chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
+	if (request.message === "selected text sent") {
+		chrome.storage.local.get(["key"], (result) => {
+			let copied_text = result.key;
+			console.log("RECIEVED: " + copied_text);
+		});
+	}
+});
