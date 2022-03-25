1670a7e5691a7e40dc4797e036d8e92fc230de93
Alex Hetrick <aph1777@rit.edu>
2022-03-25 00:30:26 -0400

    Documentation to popup.js

--- popup.js
+++ popup.js
@@ -1,27 +1,43 @@
+/**
+ * Global vars
+ */
+
 var copied_text = "";
 
-function setText() {
-  chrome.storage.local.get(["key"], (result) => {
-    copied_text = result.key;
-    document.getElementById("node-id").innerHTML = copied_text;
-  });
-}
+/**
+ * Listeners
+ */
 
 chrome.storage.onChanged.addListener(function (changes, namespace) {
-  for (let [key, { oldValue, newValue }] of Object.entries(changes)) {
-    console.log(
-      `Storage key "${key}" in namespace "${namespace}" changed.`,
-      `Old value was "${oldValue}", new value is "${newValue}".`
-    );
-    if (key == "key") {
-      setText();
+	// Log changes to storage when storage is changed
+	for (let [key, { oldValue, newValue }] of Object.entries(changes)) {
+		console.log(
+			`Storage key "${key}" in namespace "${namespace}" changed.`,
+			`Old value was "${oldValue}", new value is "${newValue}".`
+		);
 
-      console.log("ONCHANGED: " + copied_text);
-      break;
-    }
-  }
+		if (key == "key") {
+			setText();
+
+			console.log("ONCHANGED: " + copied_text);
+			break;
+		}
+	}
 });
 
 document.addEventListener("DOMContentLoaded", function () {
-  setText();
+	// Set the text when the DOM is loaded
+	setText();
 });
+
+/**
+ * Helper functions
+ */
+
+function setText() {
+	// Put stored text into the text box
+	chrome.storage.local.get(["key"], (result) => {
+		copied_text = result.key;
+		document.getElementById("node-id").innerHTML = copied_text;
+	});
+}
